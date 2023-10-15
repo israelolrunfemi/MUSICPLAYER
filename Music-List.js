fe5bda2db442732b03a5021ab7f51465c1dd2123
Israelolrunfemi <olorunfemiisrael2@gmail.com>
2023-10-15 07:53:32 +0100

    commplete

---
+++ Music-List.js
@@ -0,0 +1,46 @@
+
+let allMusic = [
+    {
+      name: "Alex action",
+      artist: "John Israel",
+      img: "cover-0",
+      src: "Alex action",
+    },
+    {
+      name: "A Long Way",
+      artist: "Dame Sad",
+      img: "cover-1",
+      src: "a-long-way",
+    },
+    {
+      name: "Daylight",
+      artist: "Beauz & Mark",
+      img: "cover-2",
+      src: "daylight",
+    },
+    {
+      name: "Forest-Lullaby",
+      artist: "Lesfm",
+      img: "cover-3",
+      src: "forest-lullaby"
+    },
+    {
+      name: "Lost In The City",
+      artist: "Cosmo Sheldrake",
+      img: "cover-4",
+      src: "lost-in-city-lights",
+    },
+    {
+      name: "Lost Sky",
+      artist: "Apple x Orange",
+      img: "cover-5",
+      src: "powerful-beat",
+    },
+    {
+        name: "Risk",
+        artist: "lorem",
+        img: "cover-6",
+        src: "Risk",
+      },
+    
+  ];
