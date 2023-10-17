23476e4af7670902e482498d33444e97b0036ab7
Israelolrunfemi <olorunfemiisrael2@gmail.com>
2023-10-17 20:50:16 +0100

    commit

--- Music-List.js
+++ Music-List.js
@@ -1,46 +1,46 @@
 
 let allMusic = [
-    {
-      name: "Alex action",
-      artist: "John Israel",
-      img: "cover-0",
-      src: "Alex action",
+  {
+    name: "Alex action",
+    artist: "John Israel",
+    img: "music-1",
+    src: "Alex action",
+  },
+  {
+    name: "A Long Way",
+    artist: "Dame Sad",
+    img: "music-2",
+    src: "a-long-way",
+  },
+  {
+    name: "Daylight",
+    artist: "Beauz & Mark",
+    img: "music-3",
+    src: "daylight",
+  },
+  {
+    name: "Forest-Lullaby",
+    artist: "Lesfm",
+    img: "music-4",
+    src: "forest-lullaby"
+  },
+  {
+    name: "Lost In The City",
+    artist: "Cosmo Sheldrake",
+    img: "music-5",
+    src: "lost-in-city-lights",
+  },
+  {
+    name: "Lost Sky",
+    artist: "Apple x Orange",
+    img: "music-6",
+    src: "powerful-beat",
+  },
+  {
+      name: "Risk",
+      artist: "lorem",
+      img: "music-1",
+      src: "Risk",
     },
-    {
-      name: "A Long Way",
-      artist: "Dame Sad",
-      img: "cover-1",
-      src: "a-long-way",
-    },
-    {
-      name: "Daylight",
-      artist: "Beauz & Mark",
-      img: "cover-2",
-      src: "daylight",
-    },
-    {
-      name: "Forest-Lullaby",
-      artist: "Lesfm",
-      img: "cover-3",
-      src: "forest-lullaby"
-    },
-    {
-      name: "Lost In The City",
-      artist: "Cosmo Sheldrake",
-      img: "cover-4",
-      src: "lost-in-city-lights",
-    },
-    {
-      name: "Lost Sky",
-      artist: "Apple x Orange",
-      img: "cover-5",
-      src: "powerful-beat",
-    },
-    {
-        name: "Risk",
-        artist: "lorem",
-        img: "cover-6",
-        src: "Risk",
-      },
-    
-  ];
+  
+];
