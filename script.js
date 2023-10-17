12f39ceb8e368fa1697c7c35de7401df3a42e5ee
Israelolrunfemi <olorunfemiisrael2@gmail.com>
2023-10-17 20:44:54 +0100

    final

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ window.addEventListener("load", ()=>{
 function loadMusic(indexNumb){
   musicName.innerText = allMusic[indexNumb - 1].name;
   musicArtist.innerText = allMusic[indexNumb - 1].artist;
-  musicImg.src = `image/${allMusic[indexNumb - 1].src}.jpg`;
+  musicImg.src = `Image/${allMusic[indexNumb - 1].src}.jpg`;
   mainAudio.src = `music/${allMusic[indexNumb - 1].src}.mp3`;
 }
 
